6f60c22e0a82e6da6b375b697ecea5961e25a0ba
UslanA <ademuslan@gmail.com>
2023-07-15 09:34:50 +0300

    gitLesson01

---
+++ src/Work01/C1.java
@@ -0,0 +1,10 @@
+package Work01;
+
+public class C1 {
+    //C:\Users\samsung\Desktop\QA\BootCamp\GitHup\GitHupLesson\src\Work01\C1.java
+    //C:\Users\samsung\Desktop\QA\BootCamp\GitHup\GitHupLesson
+
+
+
+
+}
---
+++ src/Work01/C2.java
@@ -0,0 +1,7 @@
+package Work01;
+
+public class C2 {
+    public static void main(String[] args) {
+        System.out.println("Desktop\\QA\\BootCamp\\GitHup\\GitHupLesson");
+    }
+}
